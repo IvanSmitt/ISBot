f81698f99c4f22088ca36267a2d4ec7df4776769
IvanSmitt <37902508+IvanSmitt@users.noreply.github.com>
2021-04-28 21:36:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -114,8 +114,8 @@ async def on_message(message):
         return
 
     if message.content.startswith('/help '):
-        text = message.content.strip('/help ')
-        await message.channel.send(text)
+        text = message.content[7:]
+        await message.channel.send(text+"\n")
         sentenses = text.split("\n")
         
         t = googletrans.Translator()
@@ -170,7 +170,7 @@ async def on_message(message):
 
 
     if message.content.startswith('/tti '):
-        text = message.content.strip('/tti ')
+        text = message.content[6:]
         await message.channel.send('Searching Photos')
         get_all_images_async_lim(text,128)
         await message.channel.send('Combining into one image')
